606c6dab869e900cac73d721d648ddd899f4c6bc
fatima abaiji <abaijif@gmail.com>
2025-03-19 12:32:06 +0000

    Add edit task functionality with form validation and rendering

--- tasks/views.py
+++ tasks/views.py
@@ -205,3 +205,16 @@ class TaskDeleteView(DeleteView):
         context['task'] = self.object
         return context
 
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            return render(request, 'tasks/edit_task.html', {'form': form, 'task': task})
+    else:
+        form = TaskForm(instance=task)
+        return render(request, 'tasks/edit_task.html', {'form': form, 'task': task})
+
